c7e0f6bfc5bd43e2fb656a6465e0d4c35e53ab4a
Gabriel <gabrielmaes@hotmail.com>
2022-10-15 11:28:30 +0200

    update project

--- js/main.js
+++ js/main.js
@@ -1,4 +1,39 @@
-const form = document.getElementById("novoItem")
-form.addEventListener("submit", () => {
+const form = document.getElementById("newItem")
+const list = document.getElementById("list")
+const itens = []
+
+form.addEventListener("submit", (event) => {
     event.preventDefault()
-    console.log("funcionou")})
+
+    const name = event.target.elements['name']
+    const quantity = event.target.elements['quantity']
+
+    createElement(name.value, quantity.value)
+
+    name.value = ""
+    quantity.value = ""
+
+})
+
+
+function createElement(name, quantity) {
+    const newItem = document.createElement('li')
+    newItem.classList.add("item")
+
+    const numberItem = document.createElement('strong')
+    numberItem.innerHTML = quantity
+    newItem.appendChild(numberItem)
+
+    newItem.innerHTML += name
+
+    list.appendChild(newItem)
+
+    const currentItem = {
+        "name": name,
+        "quantity": quantity
+    }
+
+    itens.push(currentItem)
+
+    localStorage.setItem("item", JSON.stringify(itens))
+};
